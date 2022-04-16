9f825166ab2faf737e1bc8c1c90370bdea62bc7a
Marcelo <marcelo.mfilho2804@gmail.com>
2022-04-16 19:33:41 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(json())
+app.use(cors())
+
+
+const PORT  = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("running on port " + PORT));
